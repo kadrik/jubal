b42fd94fd0a36edf08cc9c2777ad5b856557846e
danieleb <danielebarchiesi@me.com>
2013-12-07 15:38:28 +0000

    python functions to identify song

---
+++ en.py
@@ -0,0 +1,39 @@
+from pyechonest import song
+import py7D
+import json
+import os
+os.environ['ECHO_NEST_API_KEY'] = 'T3XI5Z9ADDMUM7372'
+
+def getSongInfo(s,case):
+    res7D = []
+    resEN = []
+    
+    if case==1:
+        # string contains name of the song
+        resEN = song.search(title=s)
+        #res7D = py7D.request('track', 'search', q=s, pageSize=1)
+        
+        # figure out the artist
+
+    elif case==2:
+        # string contains name of the artist
+        resEN = song.search(artist=s)
+        
+        # figure out the song
+
+    elif case==3:
+        # string contains song and artist
+        resEN = song.search(combined=s)
+    
+    if len(resEN)>0:
+        for i in range(0,len(resEN)):
+            title = resEN[i].title
+            artist_name = resEN[i].artist_name
+            print artist_name + ' - ' + title
+    else:
+        print 'song not found'
+        
+    return resEN, res7D
+    
+if __name__ == "__main__":
+    getSongInfo("so what",1)
